ed1cecb5de7c0badf0a4443e629acc9e84f033db
Dhanasri <dhanasreepothineni@gmail.com>
2022-01-28 13:42:58 +0530

    first commit

---
+++ src/main/java/com/example/demo/entity/Deposit.java
@@ -0,0 +1,53 @@
+package com.example.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+
+@Entity
+@Table(name = "desposit")
+public class Deposit {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer depoid;
+	
+	//Account Number
+	@Pattern(regexp = "[0-9 ]+")
+	@Size(max = 10)
+	@NotBlank
+	@NotNull
+	private String accountnum;
+	
+	//Amount
+	@NotBlank 
+	@Pattern(regexp = "^\\s*(?=.*[1-9])\\d*(?:\\.\\d{1,2})?\\s*$")
+	private Double amount;
+	
+	public Deposit(Integer depoId, String accountnum) {
+		super();
+	}
+	
+	public String getAccountnum() {
+		return accountnum;
+	}
+	public void setAccountnum(String accountnum) {
+		this.accountnum = accountnum;
+	}
+	public Double getAmount() {
+		return amount;
+	}
+	public double setAmount(Double amount) {
+		return this.amount = amount;
+	}
+	
+		
+}
+
---
+++ src/main/java/com/example/demo/service/CustomerService.java
@@ -0,0 +1,35 @@
+package com.example.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Account;
+import com.example.demo.entity.Customer;
+import com.example.demo.entity.Deposit;
+import com.example.demo.repository.AccountRepository;
+import com.example.demo.repository.CustomerRepository;
+import com.example.demo.repository.DepositRepository;
+
+
+@Service
+public class CustomerService {
+
+	@Autowired
+	private CustomerRepository customerRepository;
+	
+	@Autowired
+	private DepositRepository DepositRepository;
+	
+	@Autowired
+	private AccountRepository accountRepository;
+	
+	public void addUser(Customer customer) {
+		Deposit dep = customer.getDeposit();
+		Account acc = customer.getAccount();
+		DepositRepository.save(dep);
+		customerRepository.save(customer);
+		accountRepository.save(acc);
+	}
+	
+}
+
