ce9c7cfdc8190556e51b32a16c36b83e1b36a820
Dhanasri <dhanasreepothineni@gmail.com>
2022-01-28 13:56:41 +0530

    Added Database

--- src/main/java/com/example/demo/AccountController.java
+++ src/main/java/com/example/demo/AccountController.java
@@ -1,5 +1,28 @@
 package com.example.demo;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Account;
+import com.example.demo.service.AccountService;
+
+@RestController
 public class AccountController {
+	@Autowired
+	private AccountService accountService;
+	
+	@PostMapping("/{id}/account")
+	@ResponseStatus(code = HttpStatus.CREATED)
+	void addAccount(@RequestBody @Valid Account account, @PathVariable("id") Integer id) {
+		accountService.addAccount(account, id);
+	}
+	
 
 }
